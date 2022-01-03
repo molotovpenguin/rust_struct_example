79a6126d202235ad7c2fe2631a07cb34d69e314f
J.P. LaMorte <jp.lamorte@gmail.com>
2022-01-03 15:05:13 -0600

    area of a rectangle using stucts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rectl =Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rectl)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
